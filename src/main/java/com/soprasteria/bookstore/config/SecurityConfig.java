acb329d1224335995c15650215df781b0d7165a5
SabiaSRL <daniele.sabia.dev@hotmail.com>
2025-05-28 16:30:51 +0200

    feat: http basic authentication set

--- src/main/java/com/soprasteria/bookstore/config/SecurityConfig.java
+++ src/main/java/com/soprasteria/bookstore/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -20,8 +21,9 @@ public class SecurityConfig {
                         .requestMatchers("/cart/**").permitAll()
                         .requestMatchers("/order/**").permitAll()
                         .requestMatchers("/swagger-ui/index.html").permitAll()
-                        .anyRequest().permitAll())
-                .csrf(AbstractHttpConfigurer::disable);
+                        .anyRequest().anonymous())
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
